3127f893ea8d612dfa6dc00bc807d73e42ed7c58
Mimioj101 <mimioj@gmail.com>
2020-09-29 15:03:37 -0400

    add favorites through api

--- src/containers/favorite_container.js
+++ src/containers/favorite_container.js
@@ -4,27 +4,41 @@ import TrailCard from '../components/TrailCard'
 export default class FavoriteContainer extends React.Component {
     
     state = {
-        faveArray: []
+        faveArray: [],
+        favedTrails: []
     }
     
     mapFaves = () => {
-        return this.state.faveArray.map(fave => <TrailCard key={fave.trail_id} trail={fave.trail_id} />)
+        return this.state.faveArray.map(fave => 
+            fetch("http://localhost:3000/trails/" + fave.trail_id)
+            .then (resp => resp.json())
+            .then(trail => this.setState({favedTrails: [...this.state.favedTrails, trail]}))
+            // .then(console.log)
+            )
     }
 
     componentDidMount = () => {
         fetch("http://localhost:3000/favorites")
         .then(resp => resp.json())
-        .then(faves => this.setState({
-            faveArray: faves
-        }))
+        .then(faves => this.setState(() => ({faveArray: faves}),
+        () => {this.mapFaves()})
+        )
     }
+
+
+    mapFavedTrails = () => {
+        return this.state.favedTrails.map(trail => <TrailCard key={trail.id} trail={trail}/>)
+    }
+
+
+
     
     render() {
-        console.log("faves in faves", this.state.faveArray)
+        // console.log("faves in faves", this.state)
         return(
             <div>
                 <h3>Bookmarked Trails:</h3>
-                {this.mapFaves()}
+                {this.mapFavedTrails()}
             </div>
         )
     }
